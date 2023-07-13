0b3ce1eaa0dc619e240107d7bc56d9f016c3c2f5
Pankaj-Shinde-19 <pankajharishinde19@gmail.com>
2023-07-13 17:35:54 +0530

    Code changes done 1st time

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]) {
+        RWOnly obj = new RWOnly();
+
+        // Directly accessing the private member variable will result in an error
+        // obj.name = "John"; // Error: The field RWOnly.name is not visible
+
+        // Using the setter function to set the value of the name
+        obj.set_name("John");
+
+        // Using the getter function to access the value of the name
+        System.out.println("Name: " + obj.get_name());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    // getter's ->
+    public String get_name() {
+        return name;
+    }
+    // setter's ->
+    public void set_name(String Newname) {
+        name = Newname;
+    }
 }
